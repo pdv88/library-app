1a1b2167b7a5ccfb79a0d87f6f01c81381a12501
pdv88 <888pdv@gmail.com>
2022-11-09 15:05:43 -0600

    add js funtionality to addnew and cancel button

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let myLibrary = [];
+const add = document.getElementById("add")
+const popup_container = document.getElementById("popup_container")
+const cancel = document.getElementById("cancel")
+
+add.addEventListener("click", () => {
+    popup_container.classList.add("pop")
+})
+
+cancel.addEventListener("click", () => {
+    popup_container.classList.remove("pop")
+})
+
 
 function Book(name, pages, isRead) {
   this.name = name
@@ -7,5 +19,6 @@ function Book(name, pages, isRead) {
 }
 
 function addBookToLibrary() {
-        
+
 }
+
