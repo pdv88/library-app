9d0cea65c9a3a12bf28ace34ab2e0f8485a6e1e0
pdv88 <888pdv@gmail.com>
2022-11-16 20:24:30 -0600

    add function and button to toggle card style move delete button to bottom

--- script.js
+++ script.js
@@ -69,31 +69,12 @@ function displayBooks() {
     removeDivs[i].remove()
   }
   let index = 0
-  myLibrary.forEach(item => {
+
+  myLibrary.forEach((item, index) => {
     const card = document.createElement("div")
     card.classList.add("card")
     library.appendChild(card)
 
-    // ------create delete button------
-    const removeBookButton = document.createElement("button")
-    removeBookButton.classList.add("remove_book_button")
-    removeBookButton.addEventListener("click", deleteBookCard)
-    removeBookButton.textContent = "X"
-    removeBookButton.dataset.linkedArray = index
-    card.appendChild(removeBookButton)
-
-    // ------function to delete book card------
-    function deleteBookCard(){
-      let retreiveBookToRemove = removeBookButton.dataset.linkedArray
-      myLibrary.splice(parseInt(retreiveBookToRemove), 1)
-      card.remove()
-      displayBooks()
-    }
-    
-    // ------Function to toggle isRead status------
-    
-
-    
     // ------Create book card------
     const title = document.createElement("h2")
     title.textContent = item.title
@@ -111,19 +92,59 @@ function displayBooks() {
     inputText.textContent = "Have you read it?"
     card.appendChild(inputText)
     
-    const isRead = document.createElement("input")
-    isRead.setAttribute('type', 'checkbox')
+    const isRead = document.createElement("button")
+    isRead.classList.add("isReadStatus")
     if(item.isRead == true) {
       isRead.setAttribute('checked', true)
-    } 
+      card.classList.add("isReadTrue")
+      isRead.textContent = "Yes"
+    } else {
+      card.classList.add("isReadFalse")
+      item.isRead = false
+      isRead.textContent = "No"
+    }
     card.appendChild(isRead)
     
+    // ------create delete button------
+    const removeBookButton = document.createElement("button")
+    removeBookButton.classList.add("remove_book_button")
+    removeBookButton.addEventListener("click", deleteBookCard)
+    removeBookButton.textContent = "Delete Card"
+    removeBookButton.dataset.linkedArray = index
+    card.appendChild(removeBookButton)
+
+    // ------function to delete book card------
+    function deleteBookCard() {
+      let retreiveBookToRemove = removeBookButton.dataset.linkedArray
+      myLibrary.splice(parseInt(retreiveBookToRemove), 1)
+      card.remove()
+      displayBooks()
+    }
+
     index++
   })
 
+  
 }
 
-addBookToLibrary("Lord of the Rings", "Tolkien", "567", false)
-addBookToLibrary("Hobbit", "Tolkien", "123", true)
-addBookToLibrary("Atomic Habits", "Susan H", "232", false)
+document.addEventListener('click', (event) => {
+  const { target } = event;
+  
+  if (target.parentNode.classList.contains('isReadTrue')) {
+    target.parentNode.classList.remove('isReadTrue');
+    target.parentNode.classList.add('isReadFalse');
+    target.textContent = "No"
+    myLibrary[tr].isRead = false;
+  } else if (target.parentNode.classList.contains('isReadFalse')) {
+    target.parentNode.classList.remove('isReadFalse');
+    target.parentNode.classList.add('isReadTrue');
+    target.textContent = "Yes"
+    myLibrary[tr].isRead = true;
+  }
+  displayBooks();
+});
+
+addBookToLibrary("Lord of the Rings - The Fellowship of the Ring", "Tolkien", "567", false)
+addBookToLibrary("The Hobbit", "Tolkien", "123", true)
+addBookToLibrary("The Silmarillion", "Tolkien", "232", false)
 
